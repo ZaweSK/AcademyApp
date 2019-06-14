b682e72a7cbb76f2c3d3d84b4ae78077d1b5e3bf
Peter Sevcik <peter@Peter-Sevcik.local>
2019-06-14 08:37:40 +0200

    Add UIButtonX state colors

--- AcademyApp/Designables/UIButtonX.swift
+++ AcademyApp/Designables/UIButtonX.swift
@@ -8,14 +8,22 @@
 
 import UIKit
 
+@IBDesignable
 class UIButtonX: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBInspectable
+    var cornerRadius : CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
     }
-    */
-
+    
+    var disabledColor = UIColor.greyishBrown
+    var enabledColor = UIColor.pinkishRed
+    
+    override var isEnabled: Bool {
+        didSet {
+            self.backgroundColor = self.isEnabled ? enabledColor : disabledColor
+        }
+    }
 }
