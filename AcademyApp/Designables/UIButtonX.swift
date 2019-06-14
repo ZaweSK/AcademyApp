28647bbd19a92e98ca07549ac71b74b229079ae5
Peter Sevcik <peter@Peter-Sevcik.local>
2019-06-14 08:16:20 +0200

    Add UIButtonX file - button with inspectable corner radius

---
+++ AcademyApp/Designables/UIButtonX.swift
@@ -0,0 +1,21 @@
+//
+//  UIButtonX.swift
+//  AcademyApp
+//
+//  Created by Peter Sevcik on 14/06/2019.
+//  Copyright © 2019 Peter Sevcik. All rights reserved.
+//
+
+import UIKit
+
+class UIButtonX: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
