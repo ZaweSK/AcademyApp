45c51ed39b9dbce58ea3c2a4fe2109a6849dbe8f
Peter Sevcik <peter@Peter-Sevcik.local>
2019-06-14 08:53:49 +0200

    Add @IBOutlets and UISetup methods to ViewController

--- AcademyApp/ViewController.swift
+++ AcademyApp/ViewController.swift
@@ -8,18 +8,93 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+    // MARK: - View Controller's life cycle methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setUpColors()
+        setupUI()
     }
     
-    private func setUpColors(){
-        self.view.backgroundColor = UIColor.test
+    // MARK: - @IBOutlets
+    
+    @IBOutlet var horizontalLineViews: [UIView]!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var showLabel: UILabel!
+    @IBOutlet weak var doneButton: UIButtonX!
+}
+
+// MARK: - UI Setup Methods
+extension ViewController
+{
+    var textFields : [UITextField] {
+        return [emailTextField, passwordTextField]
     }
-
-
+    
+    private func setupUI(){
+        backgroundColorSetup()
+        textFieldsSetup()
+        showLabelSetup()
+        horizontalLinesSetup()
+        doneButtonSetup()
+    }
+    
+    private func backgroundColorSetup(){
+        view.backgroundColor = UIColor.almostBlack
+    }
+    
+    private func horizontalLinesSetup(){
+        horizontalLineViews.forEach {
+            $0.backgroundColor = UIColor.blackish
+        }
+    }
+    
+    private func textFieldsSetup(){
+        
+        let fontName = "MaisonNeue"
+        let font = UIFont(name: fontName, size: 16) ?? UIFont(name: "Helvetica", size: 16)!
+        
+        textFields.forEach {
+            $0.attributedPlaceholder = NSAttributedString(string: $0.placeholder ?? "", attributes: [
+                NSAttributedString.Key.foregroundColor : UIColor.brownGray,
+                NSAttributedString.Key.font : font,
+                NSAttributedString.Key.kern : 1
+                ])
+            
+            $0.textColor = UIColor.white
+        }
+    }
+    
+    private func showLabelSetup(){
+        
+        showLabel.isHidden = true
+        
+        let fontName = "MaisonNeue-Demi"
+        let font = UIFont(name: fontName, size: 13) ?? UIFont(name: "Helvetica", size: 13)!
+        
+        showLabel.attributedText = NSAttributedString(string: showLabel.text ?? "SHOW", attributes: [
+            NSAttributedString.Key.foregroundColor : UIColor.brownGray,
+            NSAttributedString.Key.font : font,
+            NSAttributedString.Key.kern : 1
+            ])
+        
+        showLabel.textAlignment = .right
+    }
+    
+    private func doneButtonSetup(){
+        doneButton.isEnabled = false
+        
+        let fontName = "MaisonNeue-Medium"
+        let font = UIFont(name: fontName, size: 16) ?? UIFont(name: "Helvetica", size: 16)!
+        
+        let attributedTitle = NSAttributedString(string: doneButton.title(for: .normal) ?? "Done", attributes: [
+            NSAttributedString.Key.font : font,
+            NSAttributedString.Key.foregroundColor : UIColor.white])
+        
+        doneButton.setAttributedTitle(attributedTitle, for: .normal)
+    }
 }
 
