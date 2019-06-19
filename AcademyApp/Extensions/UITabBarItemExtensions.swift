072c5a714666e63bfe041dcc15374e50d629d93e
Peter Sevcik <peter.sevcik@strv.com>
2019-06-19 12:49:47 +0200

    Add UITabBarItem extension

---
+++ AcademyApp/Extensions/UITabBarItemExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  UITabBarItemExtensions.swift
+//  AcademyApp
+//
+//  Created by Peter Sevcik on 19/06/2019.
+//  Copyright © 2019 Peter Sevcik. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITabBarItem {
+    func setImages(selectedImageName: String, unselectedImageName: String) {
+        self.selectedImage = UIImage(named: selectedImageName)?.withRenderingMode(.alwaysOriginal)
+        self.image = UIImage(named: unselectedImageName)?.withRenderingMode(.alwaysOriginal)
+    }
+}
