a0530cb8a12883d0823a8df904a02e4345b9e153
Peter Sevcik <peter.sevcik@strv.com>
2019-06-19 15:25:27 +0200

    Set up LecturesTableViewCell

---
+++ AcademyApp/Views/LecturesTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  LecturesTableViewCell.swift
+//  AcademyApp
+//
+//  Created by Peter Sevcik on 19/06/2019.
+//  Copyright © 2019 Peter Sevcik. All rights reserved.
+//
+
+import UIKit
+
+class LecturesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var wlectureImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
