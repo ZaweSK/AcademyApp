831f2cce7390d21251379b10be7cd4d50c8bf66b
Peter Sevcik <peter.sevcik@strv.com>
2019-06-20 11:50:57 +0200

    Update project

--- AcademyApp/Views/LecturesCollectionViewCell.swift
+++ AcademyApp/Views/LecturesCollectionViewCell.swift
@@ -21,9 +21,7 @@ class LecturesCollectionViewCell: UICollectionViewCell {
         self.layer.cornerRadius = 7
 
         setupLabel()
-        git 
 
-        print(lectureNameLabel)
         
 
         lectureNameLabel.constraints.forEach {
