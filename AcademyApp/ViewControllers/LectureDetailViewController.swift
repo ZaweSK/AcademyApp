42a2709452a64e4bbb1c03e6bf35d547c4e0022c
Peter Sevcik <peter.sevcik@strv.com>
2019-07-03 10:16:30 +0200

    Add backArrow to OverviewCollectionViewCell

--- AcademyApp/ViewControllers/LectureDetailViewController.swift
+++ AcademyApp/ViewControllers/LectureDetailViewController.swift
@@ -11,31 +11,11 @@ import UIKit
 class LectureDetailViewController: UIViewController {
 
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-//    func setupSwitch() {
-//        attendanceSwitch.onTintColor = .pinkishRed
-//        attendanceSwitch.tintColor = .black10
-//        attendanceSwitch.backgroundColor = .greyishBrownTwo
-//        attendanceSwitch.layer.cornerRadius = attendanceSwitch.frame.height / 2
-//    }
-//
-//    func fontAttributes(controlIsSelected: Bool) -> [NSAttributedString.Key: Any] {
-//        let fontColor: UIColor = controlIsSelected ? .white : .pinkishRed
-//        let attributes: [NSAttributedString.Key: Any] =  [
-//            NSAttributedString.Key.foregroundColor: fontColor,
-//            NSAttributedString.Key.font: UIFont.segmentedControlFont,
-//            NSAttributedString.Key.kern: 1
-//        ]
-//        return attributes
-//    }
-//
-//    func setupSegmentedControl() {
-//        segmentedControl.backgroundColor = .clear
-//        segmentedControl.tintColor = .pinkishRed
-//        segmentedControl.setTitleTextAttributes(fontAttributes(controlIsSelected: false), for: .normal)
-//        segmentedControl.setTitleTextAttributes(fontAttributes(controlIsSelected: true), for: .selected)
-//    }
+
 }
