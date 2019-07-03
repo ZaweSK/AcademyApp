14f08c0b0fb93e0d4b546cb09226290a5523fe4a
Peter Sevcik <peter.sevcik@strv.com>
2019-07-03 09:08:43 +0200

    Add @IBOutlets and setup methods to OverviewCollectionViewCell

--- AcademyApp/Views/LectureDetail/OverviewCollectionViewCell.swift
+++ AcademyApp/Views/LectureDetail/OverviewCollectionViewCell.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class OverviewCollectionViewCell: UICollectionViewCell, NibLoadableView {
 
+    // MARK: - @IBOutlets
+
+    @IBOutlet private weak var lectureNameLabel: UILabel!
+    @IBOutlet private weak var lectureImageView: UIImageView!
+    @IBOutlet private weak var segmentedControl: UISegmentedControl!
+
+    // MARK: - LifeCycle methods
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+    }
+}
+
+// MAKR: - Private setup methods
+
+private extension OverviewCollectionViewCell {
+
+    func setup() {
+        setupSegmentedControl()
     }
 
+    func fontAttributes(controlIsSelected: Bool) -> [NSAttributedString.Key: Any] {
+        let fontColor: UIColor = controlIsSelected ? .white : .pinkishRed
+        let attributes: [NSAttributedString.Key: Any] =  [
+            NSAttributedString.Key.foregroundColor: fontColor,
+            NSAttributedString.Key.font: UIFont.segmentedControlFont,
+            NSAttributedString.Key.kern: 1
+        ]
+        return attributes
+    }
+
+    func setupSegmentedControl() {
+        segmentedControl.backgroundColor = .clear
+        segmentedControl.tintColor = .pinkishRed
+        segmentedControl.setTitleTextAttributes(fontAttributes(controlIsSelected: false), for: .normal)
+        segmentedControl.setTitleTextAttributes(fontAttributes(controlIsSelected: true), for: .selected)
+    }
 }
