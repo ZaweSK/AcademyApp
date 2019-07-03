68f2364129954952a24ce13d6cab31ed0595e09c
Peter Sevcik <peter.sevcik@strv.com>
2019-07-03 09:02:48 +0200

    Add OverviewCollectionViewCell + xib file

---
+++ AcademyApp/Views/LectureDetail/OverviewCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  OverviewCollectionViewCell.swift
+//  AcademyApp
+//
+//  Created by Peter Sevcik on 02/07/2019.
+//  Copyright © 2019 Peter Sevcik. All rights reserved.
+//
+
+import UIKit
+
+class OverviewCollectionViewCell: UICollectionViewCell, NibLoadableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
