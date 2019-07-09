33d9d3ce242ee7b6ca1b2b1ce2f9161f7e942f19
Peter Sevcik <peter.sevcik@strv.com>
2019-07-09 08:17:32 +0200

    Add UserProgressTableViewCell

---
+++ AcademyApp/Views/Profile/UserProgressTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserProgressTableViewCell.swift
+//  AcademyApp
+//
+//  Created by Peter Sevcik on 09/07/2019.
+//  Copyright © 2019 Peter Sevcik. All rights reserved.
+//
+
+import UIKit
+
+class UserProgressTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
