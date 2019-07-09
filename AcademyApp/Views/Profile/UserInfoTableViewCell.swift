c47fe20df7182644e91f4b2d95f381ee3239bb33
Peter Sevcik <peter.sevcik@strv.com>
2019-07-09 07:00:00 +0200

    Add UserInfoTableViewCell + xib file

---
+++ AcademyApp/Views/Profile/UserInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserInfoTableViewCell.swift
+//  AcademyApp
+//
+//  Created by Peter Sevcik on 09/07/2019.
+//  Copyright © 2019 Peter Sevcik. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
