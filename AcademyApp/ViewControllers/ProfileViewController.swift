98c75321a308e9d798b1de28ee86c6a618df0fd8
Peter Sevcik <peter.sevcik@strv.com>
2019-07-09 06:43:35 +0200

    Add ProfileViewController

---
+++ AcademyApp/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ProfileViewController.swift
+//  AcademyApp
+//
+//  Created by Peter Sevcik on 09/07/2019.
+//  Copyright © 2019 Peter Sevcik. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    private enum Sections: Int, CaseIterable {
+        case userInfo = 0
+        case progress
+        case lectures
+    }
+
+    // MARK: - @IBOutlets
+
+    @IBOutlet weak var tableView: UITableView!
+
+    // MARK: - LifeCycle methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setup()
+    }
+}
+
+// MARK: - UITableViewDelegate methods
+
+extension ProfileViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
+
+// MARK: - UITableViewDatasource methods
+
+extension ProfileViewController: UITableViewDelegate {
+
+}
+
+// MARK: - Private setup methods
+
+private extension ProfileViewController {
+
+    func setup() {
+        setupTableView()
+    }
+
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
